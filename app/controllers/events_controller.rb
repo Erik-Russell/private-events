37dc3ba903e236485acd9cc3f05e6988390084b8
Erik-Russell <erik.z.russell@gmail.com>
2024-11-12 15:38:59 -0800

    Add Create New Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,26 @@ class EventsController < ApplicationController
     render
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.create(event_params)
+    redirect_to events_path
+  end
+
+  private
+
+  def require_login
+    if user_signed_in?
+      # allow user to perform actions
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def event_params
+    params.expect(event: [:location, :date])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
