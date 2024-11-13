faa254c8f3bec5743f6187d5504fa19870953eca
Erik-Russell <erik.z.russell@gmail.com>
2024-11-13 09:20:39 -0800

    Users #Show
    includes list of all created events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   resources :events, only: [:index, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
